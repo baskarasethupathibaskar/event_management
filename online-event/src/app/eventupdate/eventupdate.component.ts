a9c7c8ca126502079b0fa7e86920ac557fb22e2b
Baskara Sethupathi S <bask3022@global.chainsys.com>
2022-06-03 12:13:03 +0530

    include commit

--- online-event/src/app/eventupdate/eventupdate.component.ts
+++ online-event/src/app/eventupdate/eventupdate.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, NgForm, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder,Validators } from '@angular/forms';
 import { Apiservice1Service } from '../apiservice1.service';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
@@ -34,7 +34,7 @@ export class EventupdateComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { /* TODO document why this method 'ngOnInit' is empty */ }
   get ename() {
     return this.eForm.get('ename')!;
   }
