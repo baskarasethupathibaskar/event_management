5a092581345c8280b0e7c6b52504b54830d246e4
Baskara Sethupathi S <bask3022@global.chainsys.com>
2022-06-06 15:07:48 +0530

    include commit

--- online-event/src/app/gallery/gallery.component.ts
+++ online-event/src/app/gallery/gallery.component.ts
@@ -1,37 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormBuilder } from '@angular/forms';
 import { Apiservice1Service } from '../apiservice1.service';
-import { Router } from '@angular/router';
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent implements OnInit {
-  imgForm: FormGroup;
-  userRecord: any = {
-    upload: '',
-  };
+
   data: any;
   formGroup: any;
   totalUseage: any;
   alluserData: any;
   alluser: any;
   empRecord: any;
-  constructor(private fb: FormBuilder, private api: Apiservice1Service, private router:Router) {
+  constructor(private api: Apiservice1Service) {
    this.saving()
-    this.imgForm = this.fb.group({
-      upload: [this.userRecord.upload],
-    });
   }
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
 
-  
-  get upload() {
-    return this.imgForm.get('upload')!;
-  }
   
 
   saving() {
