1cffbd500ea41a0a0b192eb8e9c6d6b6d7c2a3d5
Baskara Sethupathi S <bask3022@global.chainsys.com>
2022-06-06 14:46:15 +0530

    include commit

--- online-event/src/app/service/service.component.ts
+++ online-event/src/app/service/service.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormBuilder } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 import { Apiservice1Service } from '../apiservice1.service';
 import { Router } from '@angular/router';
 @Component({
@@ -8,12 +8,6 @@ import { Router } from '@angular/router';
   styleUrls: ['./service.component.css']
 })
 export class ServiceComponent implements OnInit {
-  eForm: FormGroup;
-  userRecord: any = {
-    ename: '',
-    ereview: '',
-    eamount: '',
-  };
   data: any;
   formGroup: any;
   totalUseage: any;
@@ -22,28 +16,13 @@ export class ServiceComponent implements OnInit {
   empRecord: any;
   constructor(private fb: FormBuilder, private api: Apiservice1Service, private router:Router) {
    this.saving()
-
-    this.eForm = this.fb.group({
-      ename: [this.userRecord.ename],
-      ereview: [this.userRecord.ereview],
-      eamount: [this.userRecord.eamount],
-    });
   }
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
 
   
-  get ename() {
-    return this.eForm.get('ename')!;
-  }
-  get ereview() {
-    return this.eForm.get('ereview')!;
-  }
-  get eamount() {
-    return this.eForm.get('eamount')!;
-  }
-
+ 
   saving( ) {
     
     let data = {
