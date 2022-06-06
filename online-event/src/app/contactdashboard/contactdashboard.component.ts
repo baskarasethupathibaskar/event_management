91cc77446ab0cda301071272fbdaf994bfd35b7d
Baskara Sethupathi S <bask3022@global.chainsys.com>
2022-06-06 14:06:45 +0530

    include commit

--- online-event/src/app/contactdashboard/contactdashboard.component.ts
+++ online-event/src/app/contactdashboard/contactdashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormBuilder} from '@angular/forms';
+import {FormBuilder} from '@angular/forms';
 import { Apiservice1Service } from '../apiservice1.service';
 import { Router } from '@angular/router';
 @Component({
@@ -8,60 +8,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./contactdashboard.component.css']
 })
 export class ContactdashboardComponent implements OnInit {
-  contactForm: FormGroup;
-  AppService: any;
-  static find: any;
-  array:any = [];
-  userRecord: any = {
-  name: '',
-  email: '',
-  mobile: '',
-  adhar: '',
-  address: '',
- };
-  data: any;
-  response: any;
-  formGroup: any;
-  totalUseage: any;
   alluserData: any;
-  alluser: any;
   empRecord: any;
-
+  alluser: any;
   constructor(private fb: FormBuilder, private api: Apiservice1Service, private router:Router) {
-    this.saving( )
-
-
-    this.contactForm = this.fb.group({
-      name: [this.userRecord.name],
-      email: [this.userRecord.email],
-      mobile: [this.userRecord.mobile],
-      adhar: [this.userRecord.adhar],
-      address: [this.userRecord.address],
-    });
   }
 
   ngOnInit(): void {
     this.saving()
-
   }
-  get name() {
-    return this.contactForm.get('name')!;
-  }
-  get address() {
-    return this.contactForm.get('address')!;
-  }
-  get email() {
-    return this.contactForm.get('email')!;
-  }
-  get mobile() {
-    return this.contactForm.get('mobile')!;
-  }
-  get adhar() {
-    return this.contactForm.get('adhar')!;
-  }
-
   saving( ) {
-   
      let data = {
       selector: {
         type: "event"
